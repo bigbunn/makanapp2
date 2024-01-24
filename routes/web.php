49b617c0f0c0d53a8cb1a2d371a251baa00af518
Syaibani <syaibani4@gmail.com>
2024-01-25 06:46:29 +0700

    edit dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
     Route::get('/dashboard', function () {
