89aca71510610266287e38c74880fcc233785616
Syaibani <syaibani4@gmail.com>
2024-01-24 21:59:12 +0700

    sidebar edit

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -3,9 +3,20 @@
     <!-- Add Sidebar Menu Items Here -->
 
     <x-maz-sidebar-item name="Dashboard" :link="route('dashboard')" icon="bi bi-grid-fill"></x-maz-sidebar-item>
-    <x-maz-sidebar-item name="Component" icon="bi bi-stack">
-        <x-maz-sidebar-sub-item name="Accordion" :link="route('components.accordion')"></x-maz-sidebar-sub-item>
-        <x-maz-sidebar-sub-item name="Alert" :link="route('components.alert')"></x-maz-sidebar-sub-item>
+    <x-maz-sidebar-item name="Data Makan" icon="bi bi-archive">
+        <x-maz-sidebar-sub-item name="Buat Baru" :link="route('components.accordion')"></x-maz-sidebar-sub-item>
+        <x-maz-sidebar-sub-item name="Lihat Data" :link="route('components.accordion')"></x-maz-sidebar-sub-item>
     </x-maz-sidebar-item>
-
+    <x-maz-sidebar-item name="Pantangan" icon="bi bi-stack">
+        <x-maz-sidebar-sub-item name="Ajukan" :link="route('components.accordion')"></x-maz-sidebar-sub-item>
+        <x-maz-sidebar-sub-item name="Lihat Data" :link="route('components.alert')"></x-maz-sidebar-sub-item>
+    </x-maz-sidebar-item>
+    <x-maz-sidebar-item name="Keluhan" icon="bi bi-info">
+        <x-maz-sidebar-sub-item name="Laporkan" :link="route('components.accordion')"></x-maz-sidebar-sub-item>
+        <x-maz-sidebar-sub-item name="Lihat Data" :link="route('components.alert')"></x-maz-sidebar-sub-item>
+    </x-maz-sidebar-item>
+    <x-maz-sidebar-item name="Puasa" icon="bi bi-clock">
+        <x-maz-sidebar-sub-item name="Daftar" :link="route('components.accordion')"></x-maz-sidebar-sub-item>
+        <x-maz-sidebar-sub-item name="Lihat Data" :link="route('components.alert')"></x-maz-sidebar-sub-item>
+    </x-maz-sidebar-item>
 </x-maz-sidebar>
